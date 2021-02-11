8147f603c808717a108e32cf4331e3fe0f71fab6
Millie Senecal <millie.senecal@gmail.com>
2021-02-11 11:29:33 -0500

    set up views for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  # before_action :authenticate_user!
-  has_many :tenants
+  has_many :tenants, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :address, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pages#home'
-
   resources :tenants, except: [:show]
   resources :receipts, except: [:delete, :edit, :update, :show]
 end
