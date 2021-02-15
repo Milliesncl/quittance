8adc6ae015cf09a95653bdeb8737e85bbc5de0e6
Millie Senecal <millie.senecal@gmail.com>
2021-02-15 14:01:33 -0500

    Css homepage

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -2,6 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      blue: {
+      400: '#006D77',
+      300: '#83C5BE',
+      200: '#EDF6F9',
+      },
+      white: '#ffffff',
+      gray: '#f7f7f7',
+    },
     extend: {},
   },
   variants: {
